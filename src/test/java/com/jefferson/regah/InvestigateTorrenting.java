fdee7984fac9961f9d385bd60cccdb8125b6f59c
Comnir <comnir@gmail.com>
2017-10-14 21:17:31 +0300

    Replace jlibtorrent, because it's more low-level than needed. Use ttorrent instead, which offers high-level API and fully Java, which makes customization easier. Added an "ttorrent invastigation" class to check how ttorrent works and for future games with the library, for further customizations.

---
+++ src/test/java/com/jefferson/regah/InvestigateTorrenting.java
@@ -0,0 +1,173 @@
+package com.jefferson.regah;
+
+import com.turn.ttorrent.client.Client;
+import com.turn.ttorrent.client.SharedTorrent;
+import com.turn.ttorrent.common.Torrent;
+import com.turn.ttorrent.tracker.TrackedTorrent;
+import com.turn.ttorrent.tracker.Tracker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.*;
+import java.nio.file.*;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class InvestigateTorrenting {
+    private static final Logger log = LogManager.getLogger(InvestigateTorrenting.class);
+
+    private final InetAddress localAddress;
+    private final URI uri;
+    private final int trackerPort;
+    private final AtomicBoolean downloaderIsDone;
+
+    private InvestigateTorrenting(String ip, int trackerPort) throws URISyntaxException, UnknownHostException {
+        this.trackerPort = trackerPort;
+        this.localAddress = InetAddress.getByName(ip);
+        final String announceUrl = "http://" + ip + ":" + trackerPort + "/announce";
+        uri = new URI(announceUrl);
+        downloaderIsDone = new AtomicBoolean(false);
+    }
+
+    private void createTorrentFile(final File sharedPath, final File outputTorrent)
+            throws InterruptedException, NoSuchAlgorithmException, IOException {
+        final Torrent newTorrent = SharedTorrent.create(sharedPath, uri, "regah");
+        try (final OutputStream os = new FileOutputStream(outputTorrent)) {
+            newTorrent.save(os);
+        }
+    }
+
+    private void seedTorrent(final File torrentFile, final File parentOfShared,
+                             final int seedTime) throws IOException, NoSuchAlgorithmException {
+        final SharedTorrent torrent = SharedTorrent.fromFile(torrentFile, parentOfShared);
+        Client client = new Client(localAddress, torrent);
+
+        client.addObserver((observable, data) -> {
+            Client client1 = (Client) observable;
+            float progress = client1.getTorrent().getCompletion();
+            log.debug("Seeder# Progress update: " + progress);
+        });
+
+        log.info("seeder# starts seeding");
+        client.share(seedTime);
+
+        waitForDownloader();
+        log.debug("Seeder# is stopping");
+        client.stop(false);
+        log.debug("Seeder# is Done");
+    }
+
+    private void downloadTorrent(final File torrentFile, final File destination) throws IOException, NoSuchAlgorithmException {
+        final SharedTorrent torrent = SharedTorrent.fromFile(torrentFile, destination);
+        Client client = new Client(localAddress, torrent);
+
+        client.addObserver((observable, data) -> {
+            Client client1 = (Client) observable;
+            float progress = client1.getTorrent().getCompletion();
+            log.debug("Downloader# Progress update: " + progress);
+        });
+
+        log.info("donwloader# starts download");
+        client.download();
+        client.waitForCompletion();
+        downloaderIsDone.set(true);
+        log.debug("Downloader# is stopping");
+        client.stop(false);
+        log.debug("Downloader# is Done");
+    }
+
+    private void runTrackerSharingTorrent(final File torrentFile) throws IOException, NoSuchAlgorithmException {
+        final Tracker tracker = new Tracker(new InetSocketAddress(localAddress, trackerPort));
+
+        log.info("tracker# will announce");
+        tracker.announce(TrackedTorrent.load(torrentFile));
+
+        log.info("tracker# start");
+        tracker.start();
+        waitForDownloader();
+
+        log.info("tracker# stopping");
+        tracker.stop();
+
+        log.info("tracker# is done");
+    }
+
+    private void waitForDownloader() {
+        while (!downloaderIsDone.get()) {
+            try {
+                Thread.sleep(TimeUnit.SECONDS.toMillis(10));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws URISyntaxException, IOException, NoSuchAlgorithmException, InterruptedException {
+        final InvestigateTorrenting investigator = new InvestigateTorrenting("127.0.0.1", 9000);
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        final File sharedFile = File.createTempFile("sharedFile", "txt", null);
+        final Path source = Paths.get(investigator.getClass()
+                .getResource("/sharedFoldersBasicSharingTest/ForSharing.txt")
+                .getPath());
+        Files.copy(source, sharedFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        sharedFile.deleteOnExit();
+
+        final File torrentFile = File.createTempFile("singleFile", "torrent");
+        torrentFile.deleteOnExit();
+
+        investigator.createTorrentFile(sharedFile, torrentFile);
+
+        executorService.submit(() -> {
+            try {
+                investigator.runTrackerSharingTorrent(torrentFile);
+            } catch (IOException e) {
+                log.error("IO Exception in tracker", e);
+            } catch (NoSuchAlgorithmException e) {
+               log.error("Internal error in tracker", e);
+            }
+        });
+
+        executorService.submit(() -> {
+            try {
+                investigator.seedTorrent(torrentFile, sharedFile.getParentFile(), 300);
+            } catch (IOException e) {
+                log.error("IO Exception in tracker", e);
+            } catch (NoSuchAlgorithmException e) {
+                log.error("Internal error in seeder", e);
+            }
+        });
+
+        final File tempDestination = torrentFile.getParentFile();
+        executorService.submit(() -> {
+            try {
+                investigator.downloadTorrent(torrentFile, tempDestination);
+            } catch (IOException e) {
+                log.error("IO Exception in tracker", e);
+            } catch (NoSuchAlgorithmException e) {
+                log.error("Internal error in seeder", e);
+            }
+        });
+
+        investigator.waitForDownloader();
+        log.info("Main# downloader finished, shutting down executor");
+        executorService.shutdown();
+        final int waitSeconds = 15;
+        if (!executorService.awaitTermination(waitSeconds, TimeUnit.SECONDS)) {
+            log.info("Main# executor didn't shutdown after " + waitSeconds + " seconds");
+            executorService.shutdownNow();
+            if (!executorService.awaitTermination(waitSeconds, TimeUnit.SECONDS)) {
+                log.warn("Some tasks didn't finish while trying to shutdown!");
+            }
+        }
+        log.info("Main# is done");
+    }
+}
