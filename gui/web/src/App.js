75eb7ffa97775da7df941b49c2e2b4141e2fbb68
Nir <comnir@gmail.com>
2018-04-06 18:59:19 +0300

    Some more web GUI games, not usable for much of anything...

--- gui/web/src/App.js
+++ gui/web/src/App.js
@@ -6,10 +6,10 @@ class Resources extends React.Component {
     super(props);
     this.state = {list: []};
   }
-
+  // https://facebook.github.io/react-native/docs/network.html
   listResourcesFrom(ip) {
-    fetch('http://' + ip + ':42424/listShared')
-      .then(results => { console.log('Got result:'); return results.json() })
+    fetch('http://' + ip + ':42424/listShared', { method: 'GET' })
+      .then(results => { console.log('Got result :'); return results.json() })
       .then(data => {
         console.log('Path ' + data);
         this.setState({
@@ -20,20 +20,46 @@ class Resources extends React.Component {
   }
 
   componentWillMount() {
-    this.listResourcesFrom('127.0.0.1');
+    this.listResourcesFrom('localhost');
   }
 
   render() {
      return (
-    <select multiple>
+     <p>
+
+    <select multiple id='files' onChange={function (event) {window.alert('changed!' + [event.target.selectedOptions].map(o => o.value).join(', ')) }}>
       {
         this.state.list.map((resource) => {
           return (<option key={resource.path} value={resource.path}>{resource.path}</option>);
         })
       }
     </select>
+
+    <button type="button" onClick={function () {
+      var files = document.getElementById('files');
+      window.alert(files.selectedOptions);
+
+    }}>Click Me!</button>
+    </p>
   );
   }
+}
+
+class InputPath extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          value: null,
+        }
+    }
+    render() {
+      return (
+        <div className="InputPath">
+          Full path to file or folder: <input id="inputText" type="text"/>
+                  <input id="addFile" type="button" onClick="" value="Add"/>
+        </div>
+      )
+    }
 }
 
 class App extends Component {
@@ -46,12 +72,18 @@ class App extends Component {
         </header>
         <p className="Description">
           Select files to download from the list below.
+
+
         </p>
         <Resources />
         <p className="Block">
             -= TODO: add a destination field <br/>
             -= TODO: add a download button for the selected files
+            -= TODO: how to send file path to local server? for adding shared resources
         </p>
+        <input id="inputFile" type="file" />
+        <br/>
+        <InputPath />
       </div>
     );
   }
