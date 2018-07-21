6d1e1753c26e9a18a88a0378a83d3ddd6e5209a3
Nir <comnir@gmail.com>
2018-07-21 20:19:40 +0300

    IN-PROGRESS: some fixes to make it possible to run a full scenario on localhost only from web GUI.

--- gui/web/src/App.js
+++ gui/web/src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 class Resources extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {list: []};
+    this.state = {
+        list: [],
+        destination: ""
+        };
+    this.askForTransportData = this.askForTransportData.bind(this);
+    this.askForTransportDataWithSinglePath = this.askForTransportDataWithSinglePath.bind(this);
+    this.downloadWithTransportData = this.downloadWithTransportData.bind(this);
+    this.updateDestination = this.updateDestination.bind(this);
   }
   // https://facebook.github.io/react-native/docs/network.html
   listResourcesFrom(ip) {
@@ -24,6 +31,71 @@ class Resources extends React.Component {
     .catch((reason) => console.log(reason.toString()));
   }
 
+  downloadWithTransportData(ip, data) {
+     console.log("Starting downloadWithTransportData with transport data: " + data);
+     fetch('http://' + ip + ':42421/download', {
+                  method: 'POST',
+                  mode: 'cors',
+                  headers: {
+                              "Content-type": "application/json; charset=utf-8"
+                          },
+                  body: JSON.stringify(
+                      {
+                         "path":"/home/jefferson/Documents/regahGames/dest/", // add text field and get it's value
+                         "downloadData":JSON.stringify(data)
+                      }
+                  )
+              }
+            )
+           .then(results => {
+             console.log('Got result :');
+             return results.json() })
+         .catch((reason) => console.log(reason.toString()));
+   }
+
+  // https://facebook.github.io/react-native/docs/network.html
+  askForTransportData() {
+    var selectedOptions = document.getElementById('files').selectedOptions;
+    var selectedFiles = [].slice.call(selectedOptions)
+        .map(function(el){
+            return el.value
+         });
+
+
+    var askSingle = this.askForTransportDataWithSinglePath;
+
+    selectedFiles.forEach(function(path) {
+        askSingle('localhost', path);
+    })
+  }
+
+  askForTransportDataWithSinglePath(ip, path) {
+    console.log("Starting askForTransportDataWithPath with file to download: " + path);
+    fetch('http://' + ip + ':42424/prepareResourceForDownload', {
+             method: 'POST',
+             mode: 'cors',
+             headers: {
+                         "Content-type": "application/json; charset=utf-8"
+                     },
+             body: JSON.stringify({"filePath":path})
+         }
+       )
+      .then(results => { console.log('Got result :'); return results.json() })
+      .then(data => {
+              this.downloadWithTransportData(ip, data);
+            })
+
+    .catch((reason) => console.log(reason.toString()));
+  }
+
+  updateDestination(event) {
+    console.log("updated destination: " + event.target.value);
+    this.setState({
+              destination: event.target.value,
+            });
+  }
+
+
   componentWillMount() {
     this.listResourcesFrom('localhost');
   }
@@ -40,12 +112,20 @@ class Resources extends React.Component {
       }
     </select>
 
+    <br/>
+    <button type="button" onClick={this.askForTransportData}>Download selected files to</button>
+    <br/>
+    // consider using Electron instead to display the GUI.
+    // file manipulation: https://ourcodeworld.com/articles/read/106/how-to-choose-read-save-delete-or-create-a-file-with-electron-framework
+    <input id="destination" type="text"  onChange={this.updateDestination}/>
+    <br/>
     <button type="button" onClick={function () {
       var selectedFiles = document.getElementById('files').selectedOptions;
       window.alert([].slice
-        .call(selectedFiles).map(function(el){return el.value}));
-
-    }}>Click Me!</button>
+        .call(selectedFiles).map(function(el){
+            return el.value}));
+        }
+    }>alert</button>
     </p>
   );
   }
@@ -71,12 +151,14 @@ class InputPath extends Component {
         alert("Get path: " + this.state.value)
 
         fetch('http://localhost:42421/add', {
-                 method: 'POST',
-                 mode: 'cors',
-                 headers: {
-                             "Content-type": "application/json; charset=utf-8"
-                         },
-                 body: JSON.stringify({"paths":[this.state.value]})})
+                     method: 'POST',
+                     mode: 'cors',
+                     headers: {
+                                 "Content-type": "application/json; charset=utf-8"
+                             },
+                     body: JSON.stringify({"paths":[this.state.value]})
+                 }
+              )
               .then(results => { console.log('Got result :'); return results.json() })
               .then(data => {
                 console.log('Path ' + this.state.value);
@@ -116,7 +198,6 @@ class App extends Component {
             -= TODO: add a download button for the selected files
             -= TODO: how to send file path to local server? for adding shared resources
         </p>
-        <input id="inputFile" type="file" />
         <br/>
         <InputPath />
       </div>
